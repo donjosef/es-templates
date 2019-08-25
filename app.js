d75b4b2baee99cce04fb7114c83c8e1a17117d99
donjoe <giumontanaro@gmail.com>
2019-08-25 15:56:56 +0200

    Serve static css files from public

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const bodyParser = require('body-parser');
@@ -10,6 +12,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(homeRoutes);
