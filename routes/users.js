e6da8f954bb75a23f9c34342fcadf5dc77e2bf3e
donjoe <giumontanaro@gmail.com>
2019-08-25 12:44:38 +0200

    Render users page for /users route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/users', (req, res, next) => {
-    res.send('<h1>Users page</h1>');
+    res.render('users');
 });
 
 module.exports = router;
