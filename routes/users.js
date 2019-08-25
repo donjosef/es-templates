bf9fab0272188a01285abe15a687b21a2902f72b
donjoe <giumontanaro@gmail.com>
2019-08-25 14:35:50 +0200

    Push request body onto the users array

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+const users = [];
+
 router.get('/users', (req, res, next) => {
     res.render('users');
 });
 
 router.post('/users', (req, res, next) => {
+    users.push(req.body);
     res.redirect('/');
 });
 
