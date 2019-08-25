cdb38fd55141ebc446fe5b792c27e0f001750a07
donjoe <giumontanaro@gmail.com>
2019-08-25 12:41:43 +0200

    Render home template for home route

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Home page</h1>')
+    res.render('home');
 });
 
 module.exports = router;
